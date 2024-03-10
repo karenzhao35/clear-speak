7e5fa44495ae2ea50de93ea98b72c9520a738468
Reynella Liebig <reynella.liebig@gmail.com>
2024-03-09 20:51:41 -0800

    added background, template, and simple buttons.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
-import logo from './logo.svg';
+import React, {useState} from "react";
+import neswpaper_image from './newspaper.svg';
+import upload_image from './cloud-upload.svg'
+import summarize_image from './pencil.svg'
 import './App.css';
 
 function App() {
   const [data, setData] = React.useState(null);
+  const [message, setMessage] = useState("");
+  const [message2, setMessage2] = useState("");
+
+  const uploadFunction = () => {
+    setMessage('Hello World');
+  }
+
+  const summarizeFunction = () => {
+    setMessage2('Hello World2');
+  }
 
   React.useEffect(() => {
     fetch("/api")
@@ -11,13 +23,29 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
-  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      <div className="App-background">
+        <div className='App-template'>
+          <div className='child1'>
+            <div className='logo-header'>
+              <img src={neswpaper_image}/>
+              <div className='logo-text'>ClearSpeak</div>
+            </div>
+            <div className='catchphrase'>Condense & Comprehend Knowledge</div>
+            <button onClick={uploadFunction} className="upload-button">
+              <img src={upload_image}/> Upload File
+            </button> 
+            <p>{message}</p>
+            <button onClick={summarizeFunction} className="summarize-button">
+              <img src={summarize_image}/> Summarize
+            </button>
+            <p>{message2}</p>
+          </div>
+          <div className='child2'></div>
+        </div>
+      </div>
     </div>
   );
 }
