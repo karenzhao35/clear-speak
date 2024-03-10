9725e65ed7b1c59e589326a81219dfe8951f910a
ayesha604 <halim.aye64@gmail.com>
2024-03-09 17:22:33 -0800

    reverted to old index.js with pdfToJson.js added

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -1,21 +1,14 @@
 const express = require("express");
-const convertPdfToJson = require("./pdfToJson"); 
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-app.use(express.json()); // Middleware to parse JSON requests
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
 
-app.post("/pdf-to-json", async (req, res) => {
-    try {
-        const json = await convertPdfToJson();
-        res.json(json);
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
+  
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
--- back-end/server/pdfToJson.js
+++ back-end/server/pdfToJson.js
@@ -1,26 +1,20 @@
-const fetch = require('node-fetch');
+var data = {
+    "url": "https://www.ohchr.org/sites/default/files/UDHR/Documents/UDHR_Translations/eng.pdf",
+    "lang": "eng",
+    "inline": true,
+    "pages": "0-",
+    "async": false,
+    "name": "result.csv"
+  }
 
-async function convertPdfToJson() {
-    var data = {
-        "url": "https://www.ohchr.org/sites/default/files/UDHR/Documents/UDHR_Translations/eng.pdf",
-        "lang": "eng",
-        "inline": true,
-        "pages": "0-",
-        "async": false,
-        "name": "result.csv"
-    };
-
-    const response = await fetch('https://api.pdf.co/v1/pdf/convert/to/csv', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'x-api-key': 'halim.aye64@gmail.com_QRg6B1o99n4v115Eje0LM58I7221P0R9Q8RvR57a5b2zA3nxT16103fSUVUjAk7W'
-        },
-        body: JSON.stringify(data)
-    });
-
-    return response.json();
-}
-
-module.exports = convertPdfToJson;
+fetch('https://api.pdf.co/v1/pdf/convert/to/csv', {
+method: 'POST',
+headers: {
+'Accept': 'application/json',
+'Content-Type': 'application/json',
+'x-api-key': 'sample@sample.com_123a4b567c890d123e456f789g01'
+},
+body: JSON.stringify(data)
+})
+.then(response => response.json())
+.then(response => console.log(JSON.stringify(response)))
